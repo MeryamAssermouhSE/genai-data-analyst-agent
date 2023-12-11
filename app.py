cc973c6d688af67388895cd21cecf85629cb5f45
MeryamAssermouhSE <meryam.assermouh@artefact.com>
2023-12-11 15:24:51 +0100

    Adding database choice

--- app.py
+++ app.py
@@ -16,22 +16,11 @@ def authenticate(service_account_key):
         return None
 
 def main():
+
     st.title("Data Analyst Agent")
 
-    database_name = "movie_insights"
-
-    get_table_info_and_write_to_file(database_name, 'data/table_info.txt')
-
-    # User input: question and service account key file
-    input_question = st.text_input("Type your question:")
     uploaded_file = st.file_uploader("Upload your Google Cloud service account key JSON file", type="json")
 
-    # Using a static file for db info for now
-    with open('data/table_info.txt') as file:
-        database_info = file.read()
-
-    # the prompt to generate the query
-    engineered_prompt = " I have a database with the following informations : {data}. Using these informations, convert the following question : {question} to the simplest SQL query, destined to run in BigQuery. Give me only the SQL script as output ready to copy and paste without quotation marks and make sure to reference the database name and make sure to give meaningful aliases when needed. make sure the script is in lowercase."
 
     if uploaded_file is not None:
             try:
@@ -43,33 +32,57 @@ def main():
 
 
                 if client:
-                    # Run the analysis on button click
-                    if st.button("Run Analysis"):
 
-                        query = question_to_query(input_question=input_question, database_info=database_info, prompt_format=engineered_prompt)
-                        sql_query = query.candidates[0].text
+                    st.success("Authentication successful!")
 
-                        # Display the SQL query
-                        st.write("Generated SQL Query:")
-                        st.write(sql_query)
+                # Check if datasets are available
+                    datasets = list(client.list_datasets())
+                    if datasets:
+                        database_names = [dataset.dataset_id for dataset in datasets]
+                        database_name = st.selectbox("Select a database:", database_names)
 
-                        # Execute the query
-                        query_result = get_bq_result(sql_query)
 
-                        # Display the query result
-                        st.write("Query Result:")
-                        st.write(query_result)
+                        get_table_info_and_write_to_file(database_name, 'data/table_info.txt')
 
-                        # the prompt to geenrate the answer
-                        engineered_prompt_1 = "I run this sql query : {query_script}, and I got this result : {query_result}. Use the query result to formulate a clear and straightforward user friendly answer to this question : {question}. Give me the answer in natural language ready to be read by the end user, without quotation marks or anything"
+                        # Reading generated database info
+                        with open('data/table_info.txt') as file:
+                            database_info = file.read()
 
-                        # Create an answer
-                        answer = result_to_answer(input_question=input_question, query_script=sql_query, query_result=query_result,
-                                                prompt_format=engineered_prompt_1)
+                        # User input: question and service account key file
+                        input_question = st.text_input("Type your question:")
 
-                        # Display the final answer
-                        st.write("Final Answer:")
-                        st.write(answer.candidates[0].text)
+                        # the prompt to generate the query
+                        engineered_prompt = " I have a database with the following informations : {data}. Using these informations, convert the following question : {question} to the simplest SQL query, destined to run in BigQuery. Give me only the SQL script as output ready to copy and paste without quotation marks and make sure to reference the database name and make sure to give meaningful aliases when needed. make sure the script is in lowercase."
+
+                        if st.button("Run Analysis"):
+
+                            query = question_to_query(input_question=input_question, database_info=database_info, prompt_format=engineered_prompt)
+                            sql_query = query.candidates[0].text
+
+                            # Display the SQL query
+                            st.write("Generated SQL Query:")
+                            st.write(sql_query)
+
+                            # Execute the query
+                            query_result = get_bq_result(sql_query)
+
+                            # Display the query result
+                            st.write("Query Result:")
+                            st.write(query_result)
+
+                            # the prompt to geenrate the answer
+                            engineered_prompt_1 = "I run this sql query : {query_script}, and I got this result : {query_result}. Use the query result to formulate a clear and straightforward user friendly answer to this question : {question}. Give me the answer in natural language ready to be read by the end user, without quotation marks or anything"
+
+                            # Create an answer
+                            answer = result_to_answer(input_question=input_question, query_script=sql_query, query_result=query_result,
+                                                    prompt_format=engineered_prompt_1)
+
+                            # Display the final answer
+                            st.write("Final Answer:")
+                            st.write(answer.candidates[0].text)
+
+                    else:
+                        st.warning("No datasets available. Make sure the authenticated user has the necessary permissions.")
             except Exception as e:
                 st.error(f"Error: {e}")
 
