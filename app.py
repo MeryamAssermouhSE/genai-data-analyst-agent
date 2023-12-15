a95ef41ec6394b94fc90ad8297a378d55ac158c4
MeryamAssermouhSE <meryam.assermouh@artefact.com>
2023-12-15 09:49:04 +0100

    removing authentication

--- app.py
+++ app.py
@@ -24,106 +24,106 @@ def main():
 
     st.title("Data Analyst Agent")
 
-    uploaded_file = st.file_uploader("Upload your Google Cloud service account key JSON file", type="json")
+    #uploaded_file = st.file_uploader("Upload your Google Cloud service account key JSON file", type="json")
 
 
-    if uploaded_file is not None:
-            try:
+    #if uploaded_file is not None:
+            #try:
                 # Read the content of the uploaded service account key file
-                service_account_key_content = uploaded_file.read()
+                #service_account_key_content = uploaded_file.read()
 
                 # Authenticate with Google Cloud
-                client = authenticate(service_account_key_content)
+    client = bigquery.Client()
 
 
-                if client:
+    if client:
 
-                    st.success("Authentication successful!")
+        #st.success("Authentication successful!")
 
-                # Check if datasets are available
-                    datasets = list(client.list_datasets())
-                    if datasets:
-                        database_names = [dataset.dataset_id for dataset in datasets]
-                        database_name = st.selectbox("Select a database:", database_names)
-
-
-                        get_table_info_and_write_to_file(database_name, 'data/table_info.txt')
-
-                        # Reading generated database info
-                        with open('data/table_info.txt') as file:
-                            database_info = file.read()
-
-                        # User input: question and service account key file
-                        input_question = st.text_input("Type your question:")
-
-                        # the prompt to generate the query
-                        engineered_prompt = " I have a database with the following informations : {data}. Using these informations, convert the following question : {question} to the simplest SQL query, destined to run in BigQuery. Give me only the SQL script as output ready to copy and paste without quotation marks and make sure to reference the database name and make sure to give meaningful aliases when needed. make sure the script is in lowercase."
-                        if st.button("Dry Run"):
-                            query = question_to_query(input_question=input_question, database_info=database_info, prompt_format=engineered_prompt)
-                            sql_query = query.candidates[0].text
-
-                            # Perform a dry run to estimate bytes processed
-                            bytes_processed = dry_run_query(client, sql_query)
-
-                            st.write(f"This query will process {bytes_processed/1024:.2f} KB when run")
-                        if st.button("Run Analysis"):
-
-                            query = question_to_query(input_question=input_question, database_info=database_info, prompt_format=engineered_prompt)
-                            sql_query = query.candidates[0].text
-
-                            # Execute the query
-                            query_result = get_bq_result(sql_query)
-                            query_result = json.loads(query_result)
-
-                            n = len(query_result)
-
-                            if n<=10:
-                                # the prompt to geenrate the answer
-                                engineered_prompt_1 = "I ran this sql query : {query_script}, and I got this result : {query_result}. Use the query result to formulate a clear and straightforward user friendly answer to this question : {question}. Give me the answer in natural language ready to be read by the end user, without quotation marks or anything"
-
-                                # Create an answer
-                                answer = result_to_answer(input_question=input_question, query_script=sql_query, query_result=query_result,
-                                                        prompt_format=engineered_prompt_1)
-
-                                # Display the final answer
-                                st.write("Final Answer:")
-                                st.write(answer.candidates[0].text)
-
-                                # Display the SQL query
-                                with st.expander("View SQL Query", expanded=False):
-                                    st.write(sql_query)
-                                # Display the query result
-                                with st.expander("View Query Result", expanded=False):
-                                    st.table(query_result)
-                            
-                            else : 
-
-                                query_result = query_result[:10]
-                                                                # the prompt to geenrate the answer
-                                engineered_prompt_1 = "I ran this sql query : {query_script}, and from the result I selected the first 10 rows : {query_result}. Use this information to formulate a clear and straightforward user friendly answer to this question : {question}. Keep in mind that the complete result contains "+str(n)+" rows, and this is just a sample for reference. Give me the answer in natural language ready to be read by the end user, without quotation marks or anything"
-
-                                # Create an answer
-                                answer = result_to_answer(input_question=input_question, query_script=sql_query, query_result=query_result,
-                                                        prompt_format=engineered_prompt_1)
-
-                                # Display the final answer
-                                st.write("Summary Answer:")
-                                st.write(answer.candidates[0].text)
-
-                                # Display the SQL query
-                                with st.expander("View SQL Query", expanded=False):
-                                    st.write(sql_query)
-                                # Display the query result
-                                with st.expander("View Query Result", expanded=False):
-                                    st.write("The result contains " +str(n)+" rows. This is just a sample : ")
-                                    st.table(query_result)
+    # Check if datasets are available
+        datasets = list(client.list_datasets())
+        if datasets:
+            database_names = [dataset.dataset_id for dataset in datasets]
+            database_name = st.selectbox("Select a database:", database_names)
 
 
+            get_table_info_and_write_to_file(database_name, 'data/table_info.txt')
 
-                    else:
-                        st.warning("No datasets available. Make sure the authenticated user has the necessary permissions.")
-            except Exception as e:
-                st.error(f"Error: {e}")
+            # Reading generated database info
+            with open('data/table_info.txt') as file:
+                database_info = file.read()
+
+            # User input: question and service account key file
+            input_question = st.text_input("Type your question:")
+
+            # the prompt to generate the query
+            engineered_prompt = " I have a database with the following informations : {data}. Using these informations, convert the following question : {question} to the simplest SQL query, destined to run in BigQuery. Give me only the SQL script as output ready to copy and paste without quotation marks and make sure to reference the database name and make sure to give meaningful aliases when needed. make sure the script is in lowercase."
+            if st.button("Dry Run"):
+                query = question_to_query(input_question=input_question, database_info=database_info, prompt_format=engineered_prompt)
+                sql_query = query.candidates[0].text
+
+                # Perform a dry run to estimate bytes processed
+                bytes_processed = dry_run_query(client, sql_query)
+
+                st.write(f"This query will process {bytes_processed/1024:.2f} KB when run")
+            if st.button("Run Analysis"):
+
+                query = question_to_query(input_question=input_question, database_info=database_info, prompt_format=engineered_prompt)
+                sql_query = query.candidates[0].text
+
+                # Execute the query
+                query_result = get_bq_result(sql_query)
+                query_result = json.loads(query_result)
+
+                n = len(query_result)
+
+                if n<=10:
+                    # the prompt to geenrate the answer
+                    engineered_prompt_1 = "I ran this sql query : {query_script}, and I got this result : {query_result}. Use the query result to formulate a clear and straightforward user friendly answer to this question : {question}. Give me the answer in natural language ready to be read by the end user, without quotation marks or anything"
+
+                    # Create an answer
+                    answer = result_to_answer(input_question=input_question, query_script=sql_query, query_result=query_result,
+                                            prompt_format=engineered_prompt_1)
+
+                    # Display the final answer
+                    st.write("Final Answer:")
+                    st.write(answer.candidates[0].text)
+
+                    # Display the SQL query
+                    with st.expander("View SQL Query", expanded=False):
+                        st.write(sql_query)
+                    # Display the query result
+                    with st.expander("View Query Result", expanded=False):
+                        st.table(query_result)
+                
+                else : 
+
+                    query_result = query_result[:10]
+                                                    # the prompt to geenrate the answer
+                    engineered_prompt_1 = "I ran this sql query : {query_script}, and from the result I selected the first 10 rows : {query_result}. Use this information to formulate a clear and straightforward user friendly answer to this question : {question}. Keep in mind that the complete result contains "+str(n)+" rows, and this is just a sample for reference. Give me the answer in natural language ready to be read by the end user, without quotation marks or anything"
+
+                    # Create an answer
+                    answer = result_to_answer(input_question=input_question, query_script=sql_query, query_result=query_result,
+                                            prompt_format=engineered_prompt_1)
+
+                    # Display the final answer
+                    st.write("Summary Answer:")
+                    st.write(answer.candidates[0].text)
+
+                    # Display the SQL query
+                    with st.expander("View SQL Query", expanded=False):
+                        st.write(sql_query)
+                    # Display the query result
+                    with st.expander("View Query Result", expanded=False):
+                        st.write("The result contains " +str(n)+" rows. This is just a sample : ")
+                        st.table(query_result)
+
+
+
+        else:
+            st.warning("No datasets available. Make sure the authenticated user has the necessary permissions.")
+#except Exception as e:
+                #st.error(f"Error: {e}")
 
 
 if __name__ == "__main__":
